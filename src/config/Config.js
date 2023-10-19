59c189d66171b345866c74dd86008ea2bc5203be
Lakshay Kapoor <97797829+kapoor08@users.noreply.github.com>
2023-10-19 15:56:31 +0530

    changes

--- src/config/Config.js
+++ src/config/Config.js
@@ -11,14 +11,15 @@ import {getStorage} from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkkQ31E-MIN1r6BUbaj3e2ajTFXY5GaHI",
-  authDomain: "fir-basics-d3259.firebaseapp.com",
-  projectId: "fir-basics-d3259",
-  storageBucket: "fir-basics-d3259.appspot.com",
-  messagingSenderId: "551201580574",
-  appId: "1:551201580574:web:bc88dcef49a8ba6a7e700f",
-  measurementId: "G-22G0DK7XRY"
+  apiKey: "AIzaSyBvDo5AjXmIPftBFKQw8Gt1RazGR5StxsU",
+  authDomain: "fir-basics-1226f.firebaseapp.com",
+  projectId: "fir-basics-1226f",
+  storageBucket: "fir-basics-1226f.appspot.com",
+  messagingSenderId: "326739863283",
+  appId: "1:326739863283:web:d3d97cfd713b3e6ca37986",
+  measurementId: "G-XZRYQKZ00B"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -27,4 +28,3 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const store = getStorage(app);
 // const analytics = getAnalytics(app);
-
